02824fc7574ea0d86033839bee7d437f5f640f5f
dunguk <dunguk@gmail.com>
2016-07-15 10:01:42 -0400

    Initial Commit

---
+++ Hunt Me If You Can/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hunt Me If You Can
+//
+//  Created by Dung Nguyen Tien on 7/15/16.
+//  Copyright © 2016 David T. Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Hunt Me If You CanTests/Hunt_Me_If_You_CanTests.swift
@@ -0,0 +1,36 @@
+//
+//  Hunt_Me_If_You_CanTests.swift
+//  Hunt Me If You CanTests
+//
+//  Created by Dung Nguyen Tien on 7/15/16.
+//  Copyright © 2016 David T. Nguyen. All rights reserved.
+//
+
+import XCTest
+@testable import Hunt_Me_If_You_Can
+
+class Hunt_Me_If_You_CanTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
